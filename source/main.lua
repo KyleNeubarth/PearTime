06bcd248195c1bf3bc204a01c146b442980c5832
KyleNeubarth <kyle@neubarth.org>
2022-04-21 18:15:39 -0600

    template push

---
+++ source/main.lua
@@ -0,0 +1,19 @@
+import "Corelibs/object"
+import "Corelibs/graphics"
+import "Corelibs/sprites"
+import "Corelibs/timer"
+
+local gfx<const> = playdate.graphics
+
+local playerSprite = nil    
+
+local function init()
+    local playerImage = gfx.image.new("images/player")
+    playerSprite = gfx.sprite.new(playerImage)
+
+end
+init()
+
+function playdate.update()
+
+end
