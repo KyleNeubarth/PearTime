85c8ee72ea0b884a3dfb9025fbc4483309220d7f
KyleNeubarth <kyle@neubarth.org>
2022-04-22 08:45:45 -0600

    tutorial graphics

--- source/main.lua
+++ source/main.lua
@@ -3,6 +3,8 @@ import "Corelibs/graphics"
 import "Corelibs/sprites"
 import "Corelibs/timer"
 
+--this is a test comment! Plz push and then delete
+
 local gfx<const> = playdate.graphics
 
 local playerSprite = nil    
@@ -10,7 +12,6 @@ local playerSprite = nil
 local function init()
     local playerImage = gfx.image.new("images/player")
     playerSprite = gfx.sprite.new(playerImage)
-
 end
 init()
 
